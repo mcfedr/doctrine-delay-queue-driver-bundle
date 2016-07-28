dc215385668a01353f3262fd81971fd6118dfd9a
Fred Cox <fred@ekreative.com>
2016-07-28 18:21:46 +0300

    Allow null manager

--- src/Mcfedr/DoctrineDelayQueueDriverBundle/Entity/DoctrineDelayJob.php
+++ src/Mcfedr/DoctrineDelayQueueDriverBundle/Entity/DoctrineDelayJob.php
@@ -46,7 +46,7 @@ class DoctrineDelayJob implements Job
     /**
      * @var string
      *
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $manager;
 
